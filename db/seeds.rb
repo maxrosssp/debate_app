124e4784835482b7fdba5b98405843fdff94b2d3
Max Spiegelman <mspiegelman@wesleyan.edu>
2014-12-23 05:33:56 +0000

    Added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([{ name: 'Max', email: 'max@aol.com' }, 
+                     { name: 'Jack', email: 'jack@aol.com'},
+                     { name: 'Bill', email: 'bill@aol.com'}])
+                     
+users = [{ name: 'Marty', email: 'marty@aol.com' }, 
+        { name: 'Jan', email: 'jan@aol.com'},
+        { name: 'Brian', email: 'brian@aol.com'}]
+        
+users.each do |user|
+  User.create(user)
+end
